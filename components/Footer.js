8d67dbc82b9a639886c3121bcfb42486171967ea
spekboomh-prog <spekboomh@gmail.com>
2025-12-05 12:34:17 +0200

    Create components/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center mt-auto">
+      © {new Date().getFullYear()} CamperPixelz. All rights reserved.
+    </footer>
+  );
+}
