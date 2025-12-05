60b1a9847315eacd6d9f7c1c7071ce185d174cee
spekboomh-prog <spekboomh@gmail.com>
2025-12-05 12:29:28 +0200

    Create pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+
+      <main className="flex-1 flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Welcome to CamperPixelz!</h1>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
